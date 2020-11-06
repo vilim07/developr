69f519ac396059037c5298c1e9088034d0c76674
vilim07 <vrapic@gmail.com>
2020-11-06 11:57:22 +0100

    Update 2

---
+++ developr-under/shortcodes/boxed-posts.php
@@ -0,0 +1,34 @@
+<?php
+function boxed_posts_link(){
+    ob_start();
+    $query = new WP_Query(
+        array(
+            'post_type' => 'boxed-posts',
+            'post_status' => 'publish',
+            'order' => 'ASC',
+            'orderby' => 'menu_order',
+            'posts_per_page' => 4
+        )
+        );
+
+    if($query->have_posts()):?>
+    <div class="latest-container">
+        <?php while ($query->have_posts()):
+            $query->the_post();?>
+
+            <div class="latest">
+
+                <?php if(has_post_thumbnail()):?>
+                    <a href="<?php the_permalink(); ?>"><img src="<?php the_post_thumbnail_url(); ?>"><h1><?php the_title(); ?></h1></a>
+
+                <?php endif; ?>
+
+            </div>
+        <?php endwhile; ?>
+    </div>
+    <?php endif; ?>
+<?php
+wp_reset_query();
+return ob_get_clean();
+}
+add_shortcode('boxed_posts_link', 'boxed_posts_link');
